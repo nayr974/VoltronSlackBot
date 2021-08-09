809dff7a2a34275d5abfb587833c85c0f07b871f
Ryan Rondeau <ryan_rondeau@live.ca>
2021-08-09 11:03:12 -0700

    Add CB merge reminder

---
+++ ReminderCBMerge/__init__.py
@@ -0,0 +1,18 @@
+import datetime
+import logging
+import random
+
+import azure.functions as func
+from utils import post_message
+from .gitjokes import JOKES
+
+def main(mytimer: func.TimerRequest) -> None:
+    joke = random.choice(JOKES.splitlines())
+    post_message("#project-cb_react", text="Reminder to merge CB to master.", blocks=[{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": f"_{joke}_\n:merge: *REMINDER: It's time to merge CourseBuilder to master.* "
+			}
+		}])
+ 
