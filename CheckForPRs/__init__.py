f03b609ceb174e74a0b8c62392ae9a81b98ff4fc
Ryan Rondeau <ryan_rondeau@live.ca>
2021-09-20 08:01:52 -0700

    Add huddle notes to PR post

--- CheckForPRs/__init__.py
+++ CheckForPRs/__init__.py
@@ -14,11 +14,23 @@ def main(mytimer: func.TimerRequest) -> None:
     for item in results["items"]:
         links += f"\n•\xa0<{item.pull_request.html_url}|{item.title[:75] + '...' if len(item.title) > 78 else item.title}>"
 
-    post_message("#team-voltron", text="Reminder to review PRs.", blocks=[{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": f":blob_wave: *{results.total_count} Voltron PR's are _Ready for Review_ today!*{links}"
-			}
-		}])
+    blocks = [{
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": f":blob_wave: *{results.total_count} Voltron PR's are _Ready for Review_ today!*{links}"
+        }
+    }]
+
+    if datetime.datetime.today().weekday() in [0,2,4]:
+        blocks.append({
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": f"\n:memo: *Huddle notes in this thread! :memo:*"
+        }
+    })
+        
+    if results.total_count > 0:
+        post_message("#team-voltron", text="Reminder to review PRs.", blocks=blocks)
  
